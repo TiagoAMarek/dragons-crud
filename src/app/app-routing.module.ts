b575f61584813cf3639a88849d3d035143beb449
Tiago Marek <tiagomarek@gmail.com>
2018-10-20 17:21:02 -0300

    configure initial components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-const routes: Routes = [];
+import ListPage from '@app/pages/list/list.component'
+import LoginPage from '@app/pages/login/login.component'
+import RegisterPage from '@app/pages/register/register.component'
+
+const routes: Routes = [
+  { path: '', component: LoginPage },
+  { path: 'list', component: ListPage },
+  { path: 'register', component: RegisterPage },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+
+import ListPage from '@app/pages/list/list.component'
+import LoginPage from '@app/pages/login/login.component'
+import RegisterPage from '@app/pages/register/register.component'
+import Header from '@app/components/header/header.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    ListPage,
+    Header,
+    RegisterPage,
   ],
   imports: [
     BrowserModule,
