d3760e55b81f83a86b363166728db1079a4dc0e4
Tiago Marek <tiagomarek@gmail.com>
2018-10-20 15:18:21 -0300

    move page components into app folder

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'list-page',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export default class ListPage {
+  title = 'bla'
+}
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'register-page',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export default class RegisterPage { }
